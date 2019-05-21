8cf0dfc559b69eb379b2803ba92cfcb14c44bcec
dengzhc <2814414634@qq.com>
2019-05-21 22:23:05 +0800

    update main/main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var d = inputs.distance;
+    var df = 0;
+    if(d <= 2){
+        df = 6;
+    }else if(d > 2 && d < 8){
+        df = (d-2)*0.8 + 6;
+    }else if(d >= 8){
+        df = (d-8) * 1.2 + 10.8;
+    }
+    return Math.round(df + inputs.parkTime * 0.25);
 };
